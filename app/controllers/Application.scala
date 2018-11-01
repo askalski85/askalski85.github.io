ae40e6a23ee867162f62e8c00cf02ebfe0dcc60a
Arkadiusz Skalski <arkadiusz.skalski@tomtom.com>
2018-11-01 13:45:55 +0100

    Test DB

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,30 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
+
+import play.api.db._
 
 
-@Singleton class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+@Singleton class Application @Inject()(val database: Database, val cc: ControllerComponents) extends AbstractController(cc) {
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def db() = Action {
+    var out = ""
+    database.withConnection { conn =>
+      val stmt = conn.createStatement
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+      true
+    }
+    Ok(out)
   }
 }
--- build.sbt
+++ build.sbt
@@ -7,4 +7,10 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.12.6"
 
+// Or use cacheApi
+libraryDependencies += ehcache
+
 libraryDependencies += guice
+libraryDependencies += jdbc
+libraryDependencies += "org.postgresql" % "postgresql" % "9.4-1201-jdbc41"
+libraryDependencies += ws
